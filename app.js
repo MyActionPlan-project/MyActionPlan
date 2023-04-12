7d9837e09a53e9c518665388f128a82a591b94ec
Sotiris Ntokas <ntokas1995@live.com>
2023-04-12 11:46:59 +0200

    test commit

--- app.js
+++ app.js
@@ -18,6 +18,8 @@ require("./config")(app);
 const indexRoutes = require("./routes/index.routes");
 app.use("/api", indexRoutes);
 
+console.log("random consolelog");
+
 const authRoutes = require("./routes/auth.routes");
 app.use("/auth", authRoutes);
 
