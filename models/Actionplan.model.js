57a828484b7982c78aaf01f4d09c457d61d0b2be
Sotiris Ntokas <ntokas1995@live.com>
2023-04-14 17:02:36 +0200

    removed default value from action model

--- models/Actionplan.model.js
+++ models/Actionplan.model.js
@@ -9,7 +9,6 @@ const actionplanSchema = new Schema(
     category: {
       type: String,
       enum: ["DIY", "Vacation", "Event","Training","Other"],
-      default: "Choose",
       required: [true, "category is required."]
     },
       description:{
